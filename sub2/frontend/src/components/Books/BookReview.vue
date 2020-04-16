9cfe8e70140299f68425f4db7683855c26a4820c
김아현 <21400130@handong.edu>
2020-04-16 18:26:50 +0900

    0416 | AH | edit detail, review layout | S02P23B205-4
    Former-commit-id: 69320b0a21097ceb90bdf22575135d59e4d8da0d

--- sub2/frontend/src/components/Books/BookReview.vue
+++ sub2/frontend/src/components/Books/BookReview.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <div class="my-2 row align-items-center container">
-        <div class="col-2">{{review.username}}</div>
-        <div class="col-4 col-xl-2">
-            <div id="stars">
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
+    <div class="my-2 align-items-center container">
+        <div class="row">
+            <div class="col-2">{{review.username}}</div>
+            <div class="col-3">
+                <div id="stars">
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+            </div>
+            <div class="col-6">
+                {{review.score}}
+            </div>
+            <div class="col-1">
+                <i v-if="this.$store.getters.info.user_id==review.user" class="fas fa-trash-alt"></i>
             </div>
         </div>
-        <div class="col-6">{{review.content}}</div>
+        <div style="border-top:1px solid lightgray">{{review.content}}</div>
     </div>
 </div>
 </template>
@@ -41,5 +49,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.fa{
+    color:rgb(255, 228, 73);
+}
 </style>
